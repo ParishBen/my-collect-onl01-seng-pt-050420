c13f33ac0fca66ba9cabd613a1888ac7043db4dd
Ben Parish <parishben13@gmail.com>
2020-05-10 00:24:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+#any=['Sam Smith', 'Jessie Jackson', 'Shakira', 'J Lo']
+def my_collect(collection)
+  i=0 
+  woof = []
+  while i < collection.length
+  yield(collection[i])
 
-
+  i+=1 
+ end
+ woof
+ end
+  
